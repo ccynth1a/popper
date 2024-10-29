3fb720484c365ccee12446f064ece97ce1947aee
CCynth1a <charlottetorvell@protonmail.com>
2024-10-29 12:50:17 +0000

    Updated API

--- src/lib/components/Highscore.svelte
+++ src/lib/components/Highscore.svelte
@@ -1,32 +1,22 @@
 <script>
     import { onMount } from "svelte";
-    import { scoreStore } from "../../scoreStore";
-
-    let scores = [];
-
-    // honestly not sure what these do, they were in the tutorial
-    const unsub = scoreStore.subscribe(value => {
-        scores = value;
-    });
+    import { updateScore, fetchScore } from "../leaderboard"
+    
+    let score = $state(0);
 
     onMount(() => {
-        return () => {
-            unsub()
-        }
-    })
-
-
+        fetchScore().then(fetchedScore => {
+            score = fetchedScore;          
+        });
+    });     
 </script>
 
 <h1>Leaderboard</h1>
 
 <h2>Current Leaderboard</h2>
-<ul>
-    {#each scores as score, index}
-        <li>{index+1}: {score.time.toFixed(2)} seconds</li>
-    {/each}
-</ul>
+<p>SCORE = {score}</p>
 
+<input id="username-field" type="text">
 <style>
     
 </style>
